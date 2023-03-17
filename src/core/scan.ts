0e858d4647af073956e5fc400d4c3d245cfadb23
Jerry Wang <x.jerry.wang@gmail.com>
2023-03-17 09:49:40 +0800

    feat: support full text scan strategy

--- src/core/scan.ts
+++ src/core/scan.ts
@@ -4,7 +4,14 @@ import fg from 'fast-glob'
 import type { Options } from '../types'
 
 export default async function scanUsedSvgNames(options: Options) {
-  const { componentName, scanGlob } = options
+  const { componentName, scanGlob, iconDir, scanStrategy,symbolIdFormatter, prefix } = options
+
+  const allSvgNames = fg.sync(['**/*.svg'], { cwd: iconDir })
+                      .map(n => symbolIdFormatter!(n, prefix!))
+                      .sort((a,b) => b.length - a.length)
+
+  const testReg = new RegExp(`(${allSvgNames.join('|')})`, 'gm')
+
   const svgCompnentRE = new RegExp(
     `\\<\\s*${componentName}[^\\<]+name=[\\"\\'](\\S+)[\\"\\'][^\\<]*\\/\\>`, 'g',
   )
@@ -18,9 +25,15 @@ export default async function scanUsedSvgNames(options: Options) {
       'vite.config.js',
     ],
   })
+
   const scanExecutors = filenames.map(async (f) => {
     const code = await fs.readFile(path.resolve(process.cwd(), f), 'utf-8')
-    return Array.from(code.matchAll(svgCompnentRE)).map(match => match[1])
+
+    if (scanStrategy === 'component') {
+      return Array.from(code.matchAll(svgCompnentRE)).map(match => match[1])
+    } else {
+      return [...(code.match(testReg) || [])]
+    }
   })
 
   const svgNames = await Promise.all(scanExecutors)
